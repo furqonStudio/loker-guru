cb5ff41f62b4e482afc6d76f23b8914d8d982d57
Furqon Al-Rasyid <93087951+furqonStudio@users.noreply.github.com>
2024-10-01 22:45:22 +0700

    feat: add getUserById controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,33 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany()
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching users' })
+  }
+}
+
+export const getUserById = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: Number(id) },
+    })
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    res.json(user)
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching user' })
+  }
+}
+
 export const createUser = async (req, res) => {
   const { name, password, university, major, email, contact } = req.body
 
@@ -11,15 +38,6 @@ export const createUser = async (req, res) => {
     res.status(201).json(user)
   } catch (error) {
     res.status(500).json({ error: 'User creation failed' })
-  }
-}
-
-export const getAllUsers = async (req, res) => {
-  try {
-    const users = await prisma.user.findMany()
-    res.json(users)
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching users' })
   }
 }
 
